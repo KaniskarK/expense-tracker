54cb244b2712a08f6cb719bc5bbccd1a902713e2
KaniskarK <kaniskark@outlook.com>
2023-02-25 16:39:01 +0530

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   src/App.js
    	new file:   src/components/expense_date/ExpenseDate.css
    	new file:   src/components/expense_date/ExpenseDate.jsx
    	renamed:    src/components/expenseitem.css -> src/components/expense_item/expenseitem.css
    	new file:   src/components/expense_item/expenseitem.jsx
    	deleted:    src/components/expenseitem.jsx

---
+++ src/components/expense_item/expenseitem.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import ExpenseDate from "../expense_date/ExpenseDate";
+import "./expenseitem.css";
+const ExpenseItem = (props) => {
+   let{
+    title = ' ',
+    amount = 0,
+    date = new Date()
+   } = props;
+
+
+
+
+
+
+    return(
+    <div className="expense-item">
+        <ExpenseDate date={date}/>
+           <div className="expense-item__description">
+               <h2>{title}</h2>
+               <div className="expense-item__price">${amount}</div>
+            </div>
+    </div>
+
+  ) 
+};
+
+export default ExpenseItem;
